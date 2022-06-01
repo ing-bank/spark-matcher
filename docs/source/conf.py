9a54127a3cdb0d84b7c69dd57c5796f3e2960c58
Bayraktar <Ahmet.Bayraktar@ing.com>
2022-06-01 09:45:09 +0200

    Change conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,9 +32,6 @@ release = '0.2'
 # ones.
 extensions = ['nbsphinx', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
-napoleon_google_docstring = True
-
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
