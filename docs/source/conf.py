5d5620136aba80fd491416a7e4ce9f78bcdaae53
Frits Hermans <frits.hermans@ing.com>
2023-06-02 14:20:00 +0200

    bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Ahmet Bayraktar, Frits Hermans, Stan Leisink'
 author = 'Ahmet Bayraktar, Frits Hermans, Stan Leisink'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.4'
+release = '0.3'
 
 
 # -- General configuration ---------------------------------------------------
